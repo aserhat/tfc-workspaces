951ea5dedc6c2ba2197445d2fada7d4ad4e6bef3
Allen Serhat <serhata@gmail.com>
2020-03-29 23:33:08 -0400

    Removing workspaces to support vcs configuration.

--- modules/tfe-workspace/main.tf
+++ modules/tfe-workspace/main.tf
@@ -1,11 +1,6 @@
 resource "tfe_workspace" "workspace" {
     name  = var.workspace
     organization = var.tfe_org
-    vcs_repo {
-        identifier = var.vcs_repo_identifier
-        branch = var.vcs_repo_branch
-        oauth_token_id = var.vcs_repo_oauth_token
-    }
 }
 
 resource "tfe_variable" "tfe_org" {
--- modules/tfe-workspace/variables.tf
+++ modules/tfe-workspace/variables.tf
@@ -3,12 +3,3 @@ variable "workspace" {}
 variable "gcp_creds" {
   default = ""
 }
-variable "vcs_repo_identifier" {
-  default = ""
-}
-variable "vcs_repo_branch" {
-  default = ""
-}
-variable "vcs_repo_oauth_token" {
-  default = ""
-}

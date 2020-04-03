1feb7b711475c57f54585b6c10b2605b6ba022d0
Allen Serhat <serhata@gmail.com>
2020-04-02 23:18:16 -0400

    Test VCS Provider Switch

--- main.tf
+++ main.tf
@@ -5,12 +5,3 @@ module tfc-gcp-vcsim {
   workspace = "tfc-gcp-vcsim"
   gcp_creds = "var.gcp_creds"
 }
-
-module tfc-gcp-vcsimi-x {
-  source = "./modules/tfe-workspace"
-
-  tfe_org = "Symmetry"
-  workspace = "tfc-gcp-vcsim-x"
-  gcp_creds = "var.gcp_creds"
-}
-

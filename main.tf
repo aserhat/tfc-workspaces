3b48f24abcb19733db50896fff136f0caf5d7c65
Allen Serhat <serhata@gmail.com>
2020-04-04 14:06:40 -0400

    Add workspaces

--- main.tf
+++ main.tf
@@ -1,1 +1,31 @@
+module tfc-gcp-vcsim {
+  source = "./modules/tfe-workspace"
 
+  tfe_org = "Symmetry"
+  workspace = "tfc-gcp-vcsim"
+  gcp_creds = var.gcp_creds
+
+  vcs_repo_identifier = "aserhat/tfc-gcp-vcsim"
+  vcs_repo_branch = "master"
+  vcs_repo_oauth_token = "ot-vbGD8WZb4KC6E9dK"
+
+  terraform_version = "0.11.10"
+
+  auto_apply = true
+}
+
+module tfc-gcp-k8s {
+  source = "./modules/tfe-workspace"
+
+  tfe_org = "Symmetry"
+  workspace = "tfc-gcp-k8s"
+  igcp_creds = var.gcp_creds
+
+  vcs_repo_identifier = "aserhat/tfc-gcp-k8s"
+  vcs_repo_branch = "master"
+  vcs_repo_oauth_token = "ot-vbGD8WZb4KC6E9dK"
+
+  terraform_version = "0.11.10"
+
+  auto_apply = true
+}
